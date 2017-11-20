7553715230967817ff019b49d661378e65995bb1
Xiaozhe Yaoi <xiaozhe.yaoi@gmail.com>
2017-11-20 23:47:16 +0800

    move to v2 TensorFlow Saver

--- train.py
+++ train.py
@@ -90,7 +90,7 @@ def main(FLAGS):
             for v in tf.global_variables():
                 if not(v.name.startswith(FLAGS.loss_model)):
                     variables_to_restore.append(v)
-            saver = tf.train.Saver(variables_to_restore, write_version=tf.train.SaverDef.V1)
+            saver = tf.train.Saver(variables_to_restore, write_version=tf.train.SaverDef.V2)
 
             sess.run([tf.global_variables_initializer(), tf.local_variables_initializer()])
 
